097622418f8481fdadf56bff2ab7f040a92d0fd2
may <starmay48@gmail.com>
2019-08-22 19:43:58 +0300

    first commit

---
+++ lesson1.py
@@ -0,0 +1,14 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello, World!")
+hello, World!
+>>> exit()
+>>> if 5 > 2:
+	print("Five is greater than two!")
+
+	
+Five is greater than two!
+>>> if 5 > 2:
+print("Five is greater than two!")
+SyntaxError: expected an indented block
+>>> 
